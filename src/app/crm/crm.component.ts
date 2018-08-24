fed2fa90844d827b3818ece96adcc89481e497d1
matheus-pereira <matheus.spereira@hotmail.com>
2018-08-23 23:41:26 -0300

    correção

--- src/app/crm/crm.component.ts
+++ src/app/crm/crm.component.ts
@@ -1,6 +1,22 @@
 import { Component } from "@angular/core";
+import { UserService } from "../auth/user.service";
+import { AuthService } from "../auth/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
     templateUrl: './crm.component.html'
 })
-export class CrmComponent { }
+export class CrmComponent {
+
+    constructor(
+        private userService: UserService,
+        private authService: AuthService,
+        private router: Router
+    ) { }
+
+    logout() {
+        this.authService.signout();
+        this.userService.removeUser();
+        this.router.navigate(['']);
+    }
+}
